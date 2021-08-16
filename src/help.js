09d302b6e173f6167d0cdfc52371112afd6fc38f
FRIBOI <79055676+gabrielalmeida007@users.noreply.github.com>
2021-08-16 08:27:25 -0300

    Initial commit

---
+++ src/help.js
@@ -0,0 +1,39 @@
+const help = (pushname,prefix) => {
+return`
+𝐒𝐚𝐥𝐯𝐞 ${pushname}
+╔<══════════════════════>
+    ╔══✪〘 INFO 〙✪══
+║
+╠➥ FRIBOI 𝐁𝐎𝐓
+╠➥ 𝐃𝐎𝐍𝐎:  ғᷫʀᷢɪͥʙᷨᴏͦɪͥ ⃘⃤🐊
+╠➥ *wa.me/+5515998576105*
+╠➥ 𝐒𝐓𝐀𝐓𝐔𝐒: ON
+╠➥ 𝘱𝘳𝘦𝘧𝘪𝘹𝘰: ⌈${prefix}⌋
+╠➥ 𝘤𝘢𝘴𝘰 𝘦𝘯𝘤𝘰𝘯𝘵𝘳𝘦 𝘦𝘳𝘳𝘰𝘴: ⌈${prefix}bugreport⌋
+│╰────────────┈ ❄︎
+ ⊢🔴──◣*Menus em atualização*◥──🔴✯
+
+ ⊢───◣Private◥───✯
+│
+│⇨ ${prefix}menu2
+│ ⚠️ *Segundo menu*
+│⇨ ${prefix}menufig
+│⚠️ *Menu criar figurinhas*
+│⇨ ${prefix}plays
+│⚠️ *Menu donwload de mp3*
+│⇨ ${prefix}jogos
+│⚠️ *Games pra zoar*
+│⇨ ${prefix}menuadm
+│ ⚠️ *Administração de grupos*
+│⇨ ${prefix}menuvip
+│ ⚠️ *Menu para vips*
+│⇨ ${prefix}skiller
+│ ⚠️ *Comandos que o dono usa*
+│
+╰────⟪FRIBOI⟫────┈✯`
+
+}
+
+
+
+exports.help = help
---
+++ src/plays.js
@@ -0,0 +1,18 @@
+const plays = (prefix) => {
+	return `
+╭───────────⌁
+│  🧐🥶 _*private*_ 🥶🧐
+╘┬──────────⌁
+┌┤ 🎵 _*música*_🎵
+││
+││  *${prefix}play >mais utilizado*
+││⚠️baixa música do yt 
+││  *${prefix}mp3d >nome da música*
+││⚠️baixa música do yt
+││
+│╰──────────┈┈💫
+│ 
+╰───────────┈┈💫`
+}
+
+exports.plays = plays
